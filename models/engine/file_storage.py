0a6639c0faee86054f28678536da2078aa4e0f60
Leo-Youmbi <leoyoumbi83@gmail.com>
2023-08-11 22:58:04 +0100

    feat: adding tests for file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -25,8 +25,8 @@ class FileStorage:
             json.dump({key: value.to_dict() for key, value in my_obj.items()}, f, indent=4)
 
     def reload(self):
-        """deserializes the JSON file to __objects (only if the JSON file (__file_path) exists ; otherwise,
-        do nothing. If the file doesn’t exist, no exception should be raised)"""
+        """Deserializes the JSON file to __objects (only if the JSON file (__file_path) exists ; otherwise,
+        does nothing. If the file doesn’t exist, no exception should be raised)"""
         path = type(self).__file_path
         if os.path.exists(path):
             with open(path, "r") as f:
