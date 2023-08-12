bde13a57066e08abe311d56a661f40cf3e7a36f0
Leo-Youmbi <leoyoumbi83@gmail.com>
2023-08-12 01:42:01 +0100

    feat: adding create command to console

--- console.py
+++ console.py
@@ -2,10 +2,29 @@
 """AirBNB clone console"""
 import cmd
 
+from models.base_model import BaseModel
+
+MODELS = [BaseModel]
+
 
 class HBNBCommand(cmd.Cmd):
     """Implementation of a AirBNB CLI"""
     prompt = "(hbnb) "
+
+    def do_create(self, args):
+        """Creates an instance from the valid model argument"""
+        classnames = [cl.__name__ for cl in MODELS]
+        args = self.parse(args)
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in classnames:
+            print("** class doesn't exist **")
+        else:
+            for cl in MODELS:
+                if args[0] == cl.__name__:
+                    obj = cl()
+                    obj.save()
+                    print(obj.id)
 
     def do_EOF(self, line):
         """exit console on EOF signal (^D)"""
@@ -17,6 +36,10 @@ class HBNBCommand(cmd.Cmd):
 
     def emptyline(self) -> bool:
         pass
+
+    def parse(self, line):
+        args = line.split(" ")
+        return args
 
 
 if __name__ == '__main__':
