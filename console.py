5eedc30f3ed6edaa9484174cca0319dfb8a54eca
Leo-Youmbi <leoyoumbi83@gmail.com>
2023-08-12 03:54:52 +0100

    feat: adding destroy command to console

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """AirBNB clone console"""
 import cmd
+import models
 
 from models.base_model import BaseModel
 
@@ -15,7 +16,7 @@ class HBNBCommand(cmd.Cmd):
         """Creates an instance from the valid model argument"""
         classnames = [cl.__name__ for cl in MODELS]
         args = self.parse(args)
-        if len(args) == 0:
+        if args[0] == '':
             print("** class name missing **")
         elif args[0] not in classnames:
             print("** class doesn't exist **")
@@ -25,6 +26,42 @@ class HBNBCommand(cmd.Cmd):
                     obj = cl()
                     obj.save()
                     print(obj.id)
+                    break
+
+    def do_show(self, line):
+        """Show the string repr of an object having the given id"""
+        args = self.parse(line)
+        all_objs = models.storage.all()
+        if args[0] == '':
+            print("** class name missing **")
+        elif args[0] not in [cl.__name__ for cl in MODELS]:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing ** ")
+        else:
+            for k, v in all_objs.items():
+                if args[1] == k.split(".")[1]:
+                    print(v)
+                    return
+            print("** no instance found **")
+
+    def do_destroy(self, line):
+        """Destroy an object having the given id"""
+        args = self.parse(line)
+        all_objs = models.storage.all().copy()
+        if args[0] == '':
+            print("** class name missing **")
+        elif args[0] not in [cl.__name__ for cl in MODELS]:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing ** ")
+        else:
+            for k, v in all_objs.items():
+                if args[1] == k.split(".")[1]:
+                    models.storage.all().pop(k)
+                    models.storage.save()
+                    return
+            print("** no instance found **")
 
     def do_EOF(self, line):
         """exit console on EOF signal (^D)"""
