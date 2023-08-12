21740428fddfa3ce48cd7d1ce2bccd54c5e5c975
Leo-Youmbi <leoyoumbi83@gmail.com>
2023-08-12 16:51:46 +0100

    feat: adding amenity, city, place, review, state model and Updating the FileStorage to adapt

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,10 +3,15 @@
 import json
 import os
 
+from models.amenity import Amenity
 from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 from models.user import User
 
-MODELS = [BaseModel, User]
+MODELS = [BaseModel, User, Amenity, Place, City, Review, State]
 
 
 class FileStorage:
---
+++ models/review.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Review Module
+"""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    place_id: str = ""
+    user_id: str = ""
+    text: str = ""
